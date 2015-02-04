83879876e932adf34e4d28177a7dc6b70c1f0ff6
Cole Geissinger <colegeissinger@gmail.com>
2015-02-03 21:49:18 -0800

    Adding proper white spacing and only loading WIMP+ sub pages after you have signed up

--- includes/membership/bp-membership-loader.php
+++ includes/membership/bp-membership-loader.php
@@ -51,7 +51,7 @@ class BP_Membership_Component extends BP_Component {
 	 * @param array $includes
 	 */
 	function includes( $includes = array() ) {
-		$path = 'includes/membership/';
+		$path     = 'includes/membership/';
 		$includes = array(
 			$path . 'bp-actions.php',
 			$path . 'bp-screens.php',
@@ -88,8 +88,8 @@ class BP_Membership_Component extends BP_Component {
 
 		// Set up the $globals array to be passed along to parent::setup_globals()
 		$globals = array(
-			'slug' => BP_WIMP_MEMBERSHIP_SLUG,
-			'root_slug' => isset( $bp->pages->{$this->id}->slug ) ? $bp->pages->{$this->id}->slug : BP_WIMP_MEMBERSHIP_SLUG,
+			'slug'          => BP_WIMP_MEMBERSHIP_SLUG,
+			'root_slug'     => isset( $bp->pages->{$this->id}->slug ) ? $bp->pages->{$this->id}->slug : BP_WIMP_MEMBERSHIP_SLUG,
 			'has_directory' => false,
 		);
 
@@ -110,40 +110,42 @@ class BP_Membership_Component extends BP_Component {
 	function setup_nav( $main_nav = array(), $sub_nav = array() ) {
 		// Add the Membership parent item
 		$main_nav = array(
-			'name' => 'WIMP+',
-			'slug' => bp_get_wimp_membership_slug(),
-			'position' => 80,
-			'screen_function' => 'bp_wimp_membership_account_screen',
-			'default_subnav_slug' => 'account',
+			'name'                    => 'WIMP+',
+			'slug'                    => bp_get_wimp_membership_slug(),
+			'position'                => 80,
+			'screen_function'         => 'bp_wimp_membership_account_screen',
+			'default_subnav_slug'     => 'account',
 			'show_for_displayed_user' => false,
 		);
 
 		$sub_nav[] = array(
-			'name' => 'Account',
-			'slug' => 'account',
-			'parent_url' => trailingslashit( bp_loggedin_user_domain() . bp_get_wimp_membership_slug() ),
-			'parent_slug' => bp_get_wimp_membership_slug(),
+			'name'            => 'Account',
+			'slug'            => 'account',
+			'parent_url'      => trailingslashit( bp_loggedin_user_domain() . bp_get_wimp_membership_slug() ),
+			'parent_slug'     => bp_get_wimp_membership_slug(),
 			'screen_function' => 'bp_wimp_membership_account_screen',
-			'position' => 10,
+			'position'        => 10,
 		);
 
-		$sub_nav[] = array(
-			'name' => 'Billing',
-			'slug' => 'billing',
-			'parent_url' => trailingslashit( bp_loggedin_user_domain() . bp_get_wimp_membership_slug() ),
-			'parent_slug' => bp_get_wimp_membership_slug(),
-			'screen_function' => 'bp_wimp_membership_billing_screen',
-			'position' => 20,
-		);
+		if ( wmd_is_wimp_plus_member() ) {
+			$sub_nav[] = array(
+				'name'            => 'Billing',
+				'slug'            => 'billing',
+				'parent_url'      => trailingslashit( bp_loggedin_user_domain() . bp_get_wimp_membership_slug() ),
+				'parent_slug'     => bp_get_wimp_membership_slug(),
+				'screen_function' => 'bp_wimp_membership_billing_screen',
+				'position'        => 20,
+			);
 
-		$sub_nav[] = array(
-			'name' => 'Invoices',
-			'slug' => 'invoice',
-			'parent_url' => trailingslashit( bp_loggedin_user_domain() . bp_get_wimp_membership_slug() ),
-			'parent_slug' => bp_get_wimp_membership_slug(),
-			'screen_function' => 'bp_wimp_membership_invoice_screen',
-			'position' => 30,
-		);
+			$sub_nav[] = array(
+				'name'            => 'Invoices',
+				'slug'            => 'invoice',
+				'parent_url'      => trailingslashit( bp_loggedin_user_domain() . bp_get_wimp_membership_slug() ),
+				'parent_slug'     => bp_get_wimp_membership_slug(),
+				'screen_function' => 'bp_wimp_membership_invoice_screen',
+				'position'        => 30,
+			);
+		}
 
 		parent::setup_nav( $main_nav, $sub_nav );
 	}
@@ -152,9 +154,9 @@ class BP_Membership_Component extends BP_Component {
 /**
  * Loads the component into the $bp global
  *
- * By hooking to bp_loaded, we ensure that BP_Example_Component is loaded after Buddypress's core components.
- * This is a good thing because it gives us access to those components' functions and data, should our component
- * interactive with them.
+ * By hooking to bp_loaded, we ensure that BP_Example_Component is loaded after Buddypress's core
+ * components. This is a good thing because it gives us access to those components' functions and
+ * data, should our component interactive with them.
  *
  * @global obj $bp
  */
@@ -163,4 +165,5 @@ function bp_wimp_membership_load_core_component() {
 
 	$bp->wimp_membership = new BP_Membership_Component;
 }
+
 add_action( 'bp_loaded', 'bp_wimp_membership_load_core_component' );
